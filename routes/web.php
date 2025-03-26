e3a8ff3b0bf904efcc2fc6f4268131a94a566d5b
Bùi Tĩnh <buitinh@gmail.com>
2025-03-26 18:47:26 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\clients\HomeController;
+use App\Http\Controllers\clients\AboutController;
+use App\Http\Controllers\clients\ToursController;
+use App\Http\Controllers\clients\TravelGuidesController;
+use App\Http\Controllers\clients\DestinationController;
+use App\Http\Controllers\clients\ContactController;
+use App\Http\Controllers\clients\ErrorsController;
+use App\Http\Controllers\clients\TourDetailController;
+use App\Http\Controllers\clients\BlogController;
+use App\Http\Controllers\clients\BlogDetailController;
+
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::get(uri: '/', action: [HomeController::class, 'index' ])->name(name: 'home');
+Route::get(uri: '/about', action: [AboutController::class, 'index' ])->name(name: 'about');
+Route::get(uri: '/tours', action: [ToursController::class, 'index' ])->name(name: 'tours');
+Route::get(uri: '/travel-guides', action: [TravelGuidesController::class, 'index' ])->name(name: 'team');
+Route::get(uri: '/destination', action: [DestinationController::class, 'index' ])->name(name: 'destination');
+Route::get(uri: '/contact', action: [ContactController::class, 'index' ])->name(name: 'contact');
+Route::get(uri: '/tour-detail', action: [TourDetailController::class, 'index' ])->name(name: 'tour-detail');
+Route::get(uri: '/contact1', action: [ErrorsController::class, 'index' ])->name(name: 'contact1');
+Route::get(uri: '/blogs', action: [BlogController::class, 'index' ])->name(name: 'blogs');
+Route::get(uri: '/blog-detail', action: [BlogDetailController::class, 'index' ])->name(name: 'blog-detail');
